f724c4c495c6962d33619afb6e92b5f59141de9b
daniloalima <henriqued605@gmail.com>
2025-07-14 14:52:47 -0300

    fix:fiz cagada,arrumando

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App: React.FC = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/catalog" element={<Catalogo />} />
-        <Route path="/catalog/" element={<EditarMesa />} />
+        <Route path="/catalog/" element={<Catalogo />} />
         <Route path="/catalog/add" element={<AdicionarMesa />} />
         <Route path="/mesa/:id" element={<MesaDetail />} />
         <Route path="/mesa/:id/edit" element={<EditarMesa />} />
